01c9d7e896ac35d5e11bfe2dcb339ec9d292d709
Victoria Demina <viktoriia.demina@gmail.com>
2023-04-01 11:04:23 +0100

    Integrate with ChatGPT

--- main.py
+++ main.py
@@ -1,23 +1,30 @@
 import os
+import openai
 from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
 API_TOKEN = os.getenv("API_TOKEN")
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
 bot = Bot(token=API_TOKEN)
-dp = Dispatcher(bot)
+dp = Dispatcher(bot, storage=MemoryStorage())
 
 
 @dp.message_handler(commands=['start'])
-async def send_welcome(message: types.Message):
-    await message.reply(
-        "Hi!\n"
-        "I'm your first bot that you created!\n"
-        "What's your name?'")
+async def start_cmd_handler(message: types.Message):
+    await message.answer("Hi! I'm a ChatGenie chatbot. Ask me anything!")
 
 
 @dp.message_handler()
-async def echo(message: types.Message):
-    await message.answer("Nice to meet you!")
+async def message_handler(message: types.Message):
+    # Generate response using ChatGPT
+    response = openai.Completion.create(
+        engine='davinci',
+        prompt=message.text,
+        max_tokens=50
+    )['choices'][0]['text']
+    # Send response to user
+    await message.answer(response)
 
 
 if __name__ == '__main__':
